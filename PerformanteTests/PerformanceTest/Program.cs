082775616d7b430f768c7af2130eac4543d2a184
benmuratseker <benmuratseker@hotmail.com>
2025-03-09 22:56:15 +0100

    nbomber data test added

--- PerformanteTests/PerformanceTest/Program.cs
+++ PerformanteTests/PerformanceTest/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.VisualBasic;
-using NBomber.CSharp;
+﻿using NBomber.CSharp;
 using NBomber.Http;
-using NBomber.Http.FSharp;
+using NBomber.Http.CSharp;
+using NBomber.Data;
+using NBomber.Data.CSharp;
 
 using var httpClient = new HttpClient();
+IDataFeed<string> categories = DataFeed.Random(new List<string>{"all", "boots", "equip", "kayak"});
 
 var scenario = Scenario.Create("get_product_list", async context =>
 {
@@ -14,16 +16,36 @@ var scenario = Scenario.Create("get_product_list", async context =>
     //     ? Response.Ok()
     //     : Response.Fail();
     #endregion
+
+    #region Http Request Nbomber.Http
+    // var request = Http.CreateRequest("GET","https://localhost:7213/product?category=boots");
+    // return await Http.Send(httpClient, request);
+    #endregion
     
-    var request = Http.createRequest("GET","https://localhost:7213/product?category=boots");
-    return await Http.send(httpClient, request);
+    #region Data Nbomber.Data
+    var category = categories.GetNextItem(context.ScenarioInfo);
+    var request = Http.CreateRequest("GET",$"https://localhost:7213/product?category={category}");
+    return await Http.Send(httpClient, request);
+    #endregion
 })
 .WithoutWarmUp()
 .WithLoadSimulations(
-    Simulation.Inject(
-        rate: 100, 
-        interval:TimeSpan.FromSeconds(1), 
-        during: TimeSpan.FromSeconds(30)));
+    // Simulation.Inject(
+    //     // rate: 100, 
+    //     rate: 400, 
+    //     interval:TimeSpan.FromSeconds(1), 
+    //     during: TimeSpan.FromSeconds(30))
+
+    #region  Additional requests
+    Simulation.RampingInject(rate:250,
+        interval:TimeSpan.FromSeconds(1),
+        during: TimeSpan.FromSeconds(30)),
+    Simulation.RampingInject(rate: 100,
+            interval: TimeSpan.FromSeconds(1),
+            during: TimeSpan.FromSeconds(30))
+
+    #endregion
+);
         
 // NBomberRunner.RegisterScenarios(scenario).Run();
 // NBomberRunner.RegisterScenarios(scenario).("product_service").Run();
--- PerformanteTests/PerformanceTest/obj/Debug/net8.0/PerformanceTest.AssemblyInfo.cs
+++ PerformanteTests/PerformanceTest/obj/Debug/net8.0/PerformanceTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerformanceTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a298c005e8818020bcd9607a58d8a4a96fa8a461")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80880151d7cca5b01cd65128de880d755f731263")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerformanceTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerformanceTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
