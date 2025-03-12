6dcd5895f52962b470a2622c379b6185b56906a4
benmuratseker <benmuratseker@hotmail.com>
2025-03-12 23:51:44 +0100

    dashboard files for importing grafana

--- PerformanteTests/PerformanceTest/obj/Debug/net8.0/PerformanceTest.AssemblyInfo.cs
+++ PerformanteTests/PerformanceTest/obj/Debug/net8.0/PerformanceTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PerformanceTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebce7e8cfa0e66061f3a34349ce49de92d2d4c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c95a3108ccd4be942779f0c9009e73a8e961f380")]
 [assembly: System.Reflection.AssemblyProductAttribute("PerformanceTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PerformanceTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
